5da4aefccead5096ad5dd5b66219ca379f30ca6e
Defined Risk <DefinedRisk@outlook.com>
2020-11-16 19:07:44 +0000

    Chap9 SDI Squares recreated in VS2019
    Followed procedure pp537-539. Confirm no changes to process are required. https://docs.microsoft.com/en-us/cpp/mfc/reference/mfc-application-wizard?view=msvc-160

---
+++ Ch9/SDI Squares VS2019/Squares2019View.h
@@ -0,0 +1,48 @@
+
+// Squares2019View.h : interface of the CSquares2019View class
+//
+
+#pragma once
+
+
+class CSquares2019View : public CView
+{
+protected: // create from serialization only
+	CSquares2019View() noexcept;
+	DECLARE_DYNCREATE(CSquares2019View)
+
+// Attributes
+public:
+	CSquares2019Doc* GetDocument() const;
+
+// Operations
+public:
+
+// Overrides
+public:
+	virtual void OnDraw(CDC* pDC);  // overridden to draw this view
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// Implementation
+public:
+	virtual ~CSquares2019View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// Generated message map functions
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+};
+
+#ifndef _DEBUG  // debug version in Squares2019View.cpp
+inline CSquares2019Doc* CSquares2019View::GetDocument() const
+   { return reinterpret_cast<CSquares2019Doc*>(m_pDocument); }
+#endif
+
